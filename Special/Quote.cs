1e28ba082e853f71431e2a99a46812d2f61256cd
bobbyklmnop <bobbyklmnop@gmail.com>
2015-10-11 17:10:14 -0500

    final commit hopefully
    SO DONE!!

--- Special/Quote.cs
+++ Special/Quote.cs
@@ -15,9 +15,13 @@ namespace Tree {
 
     public class Quote : Special {
 
-        private bool isRoot = false; // whether or not this quote is the first
-        private bool charQuote = true; // if this is '() rather than (quote )
-        private int depth; // the current depth
+        // Declare variables that help determine pretty printing rules
+
+        private bool isRoot = false;    // whether or not this quote is the root
+        private bool charQuote = true;  // if this is '() rather than (quote )
+        private  int depth;             // the current depth
+
+        // Define constructors that affect the private variables differently
 
         public Quote (bool b, int d) {
             charQuote = b;
@@ -37,8 +41,16 @@ namespace Tree {
             for (int i = 0; i < n; i++)
                 Console.Write("    ");
 
+            // Since this form can be created from either the quote character,
+            // '\'', or the quote string, "quote" we need two mutually
+            // exclusive cases to handle each
 
             if (charQuote) {
+                // If what is trying to be printed is the root node,
+                // set all subsequent Cons nodes' forms to Quote
+                // print "'("
+                // if the cdr is not nil, recur down the tree
+                //else it is nil, so print the RPARN
                 if (isRoot) {
                     t.setFormToQuote(charQuote, depth);
                     t.getCar().print(0, false);
@@ -51,6 +63,13 @@ namespace Tree {
                         t.getCdr().print(0, true);
                     }
                 }
+
+                // This is not the root, so do these set of instructions:
+                // If the car is a Cons node, print a LPAREN because it is the
+                // start of an inner list.
+                // Print the car, which will implicitly recur on its subtree
+                // Print single whitespaces when necessary and
+                // print the cdr which also implicity recurs on its subtree
                 else {
                     if (t.getCar().isPair()) {
                         Console.Write("(");
@@ -61,10 +80,18 @@ namespace Tree {
                     }
                     t.getCdr().print(0, true);
                 }
+
+                // Write a carriage return only if the cdr is Nil and the
+                // Cons node depth is 0, which means it is on far right side of
+                // the tree
                 if (t.getCdr().isNil() && depth == 0) {
                     Console.WriteLine();
                 }
             }
+
+            // Quote strings are handled very similary with the exception that
+            // the first and last parentheses are not printed and "quote" will
+            // pretty print out to "\'"
             else {
                 if (isRoot) {
                     t.setFormToQuote(charQuote, depth);
